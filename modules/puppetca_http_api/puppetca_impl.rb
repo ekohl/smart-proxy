a7c682b69d70481893ae961e7c4c9854f4793198
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-02-20 17:16:57 +0100

    Normalize Puppet certificates

--- modules/puppetca_http_api/puppetca_impl.rb
+++ modules/puppetca_http_api/puppetca_impl.rb
@@ -19,12 +19,35 @@ module ::Proxy::PuppetCa::PuppetcaHttpApi
         # serial, not_before and not_after are not available via http api
         # see https://tickets.puppetlabs.com/browse/SERVER-2370
         hsh[name] = {
-          'state' => entry['state'],
+          'state' => get_state(entry),
           'fingerprint' => entry['fingerprint'],
           'serial' => nil,
           'not_before' => nil,
           'not_after' => nil
         }
+      end
+    end
+
+    private
+
+    # Normalize the state to match the Puppet < 6 command line values since
+    # that's what Foreman expects.
+    #
+    # Puppet CA API sends: requested, signed, revoked
+    # Foreman expects: pending, valid, revoked
+    def get_state(entry)
+      case entry['state']
+      when 'signed'
+        # Versions before 6.3 didn't send not_after
+        if entry['not_after'] && Time.parse(entry['not_after']) > Time.now
+          'revoked'
+        else
+          'valid'
+        end
+      when 'requested'
+        'pending'
+      else
+        entry['state']
       end
     end
   end
